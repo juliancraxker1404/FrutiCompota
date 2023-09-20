45402fcfeb86905ae24c946f215fc6bc64337e9f
Julian <juliandavidhoyosmeneses@gmail.com>
2023-09-19 23:03:01 -0500

    fix

--- JS/app.js
+++ JS/app.js
@@ -4,9 +4,9 @@ check.addEventListener('click',idioma);
 function idioma(){
     let id=check.checked;
     if(id==true){
-        location.href="http://127.0.0.1:5500/principal/paginaprincipal.html";
+        location.href="http://127.0.0.1:5501/index.html";
     }else{
-        location.href="http://127.0.0.1:5500/principal/indexingles.html";
+        location.href="http://127.0.0.1:5501/principal/indexingles.html";
     }
 }
 
